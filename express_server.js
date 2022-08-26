e36de74d3c51817d6b9fd5e7146cedcfddd6f075
Ruanruanx <zxin7@uwo.ca>
2022-08-26 18:33:31 +0000

    login check and permission to certain pages

--- express_server.js
+++ express_server.js
@@ -32,11 +32,11 @@ const users = {
   },
 };
 
-const getUserByEmail=(email)=>{
-  for(const key in users){
-    if(Object.hasOwnProperty.call(users,key)){
+const getUserByEmail = (email) => {
+  for (const key in users) {
+    if (Object.hasOwnProperty.call(users, key)) {
       const user = users[key];
-      if(user.email === email){
+      if (user.email === email) {
         return user;
       }
     }
@@ -46,64 +46,63 @@ const getUserByEmail=(email)=>{
 
 app.set("view engine", 'ejs');
 
-
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
   // const templateVars = { username: req.cookies["username"] };
-  const templateVars = {user:null};
-  res.render("register",templateVars)
+  const templateVars = { user: null };
+  res.render("register", templateVars)
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
   let id = generateRandomString(3);
   //read the name, email and password
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   //check if the email is empty
-  if(!email) return res.status(400).send('Email is empty')
+  if (!email) return res.status(400).send('Email is empty')
 
   //check if the email is taken
   const userExists = getUserByEmail(email);
-  if(userExists) return res.status(400).send('Email is already taken')
+  if (userExists) return res.status(400).send('Email is already taken')
   //add new user
   const newUser = {
     id,
     email,
     password,
   }
-  users[id]=newUser;
+  users[id] = newUser;
   //respond back to the client with a cookie
   res.cookie("user_id", id)
   res.redirect("/urls")
 })
 
-app.get("/login",(req,res)=>{
-  const templateVars = {user:null};
-  res.render("login",templateVars)
+app.get("/login", (req, res) => {
+  const templateVars = { user: null };
+  res.render("login", templateVars)
 })
 
 app.post("/login", (req, res) => {
   //retrieve input email and password
-  const{email,password} =req.body;
+  const { email, password } = req.body;
 
   //check if email exists in users database
   const user = getUserByEmail(email);
-  if(!user) return res.status(403).send('The email provided does not exist')
+  if (!user) return res.status(403).send('The email provided does not exist')
 
   //validate password match
-  if(user.password === password){
+  if (user.password === password) {
     //return cookie if password is right
     res.cookie('user_id', user.id);
     res.redirect("/urls")
   } else {
     return res.status(403).send('The password is incorrect')
   }
-//  const userId = req.cookies.user_id;
+  //  const userId = req.cookies.user_id;
 })
 
 app.post("/logout", (req, res) => {
   const userId = req.cookies.user_id
-  res.clearCookie("user_id",userId)
+  res.clearCookie("user_id", userId)
   res.redirect("/urls")
 })
 
@@ -123,18 +122,25 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
+  console.log("cookies are" , req.cookies);
   // Log the POST request body to the console
   //save  urlDatabase to id-longURL key-value pair 
+  if(req.cookies.userId){
   let shortUrl = generateRandomString(6);
   urlDatabase[shortUrl] = req.body.longURL;
-  res.redirect("http://localhost:8080/urls/" + shortUrl); // Respond with 'Ok' (we will replace this)
+  res.redirect("http://localhost:8080/urls/" + shortUrl);
+  }else{
+    return res.status(403).send("Please login first")
+  }
 });
 
-
-
 app.get("/u/:id", (req, res) => {
-  res.redirect(longURL);
+  let id = req.params.id;
+  if(urlDatabase[id]){
+    res.redirect(urlDatabase[id]);
+  } else {
+    res.status(403).send("Short url doesn't exist")
+  }
 });
 
 app.get("/", (req, res) => {
@@ -159,8 +165,13 @@ app.get("/urls/new", (req, res) => {
   // const templateVars = { username: req.cookies["username"] };
   const userId = req.cookies.user_id
   const user = users[userId]
-  const templateVars = {user};
-  res.render("urls_new", templateVars);
+  const templateVars = { user };
+  if (user) {
+    res.render("urls_new", templateVars);
+  } else {
+    //redirect to GET /login if not logged in
+    res.redirect("/login")
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -169,20 +180,23 @@ app.get("/urls/:id", (req, res) => {
   const userId = req.cookies.user_id
   const user = users[userId];
   // const templateVars = { id, longURL, username: req.cookies["username"] };
-  const templateVars = { id, longURL, user};
+  const templateVars = { id, longURL, user };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const userId = req.cookies.user_id
   const user = users[userId]
-
   const templateVars = {
     urls: urlDatabase,
     //username: req.cookies["username"]
     user
   };
+  if(!userId){
+    res.statusCode=401
+  }
   res.render("urls_index", templateVars);
+
 });
 
 app.listen(PORT, () => {
